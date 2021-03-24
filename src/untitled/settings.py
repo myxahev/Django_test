460c166414c8257a4ffcf5b568f8547e9b9f911a
Dmitry <myxahev2@gmail.com>
2021-03-24 23:34:59 +0500

    complete docker django nginx postgresql

--- src/untitled/settings.py
+++ src/untitled/settings.py
@@ -76,10 +76,12 @@ WSGI_APPLICATION = 'untitled.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'postgres',
+    'USER' : 'postgres',
+    'PASSWORD' : 'P@ssw0rd',
+    'HOST' : 'postgres',
+    'PORT' : '5432',
 }
 
 
